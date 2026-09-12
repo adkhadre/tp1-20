3f081c8831a1182e9483bbfc1ff497dba7cd8684
Abdoul Khadre Diop <adkhadre01@gmail.com>
2026-09-13 00:25:18 +0200

    Ajout des constructeurs dans la classe Fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -2,6 +2,20 @@ public class Fraction {
     private int num;
     private int den;
 
+    public Fraction(int num, int den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    public Fraction(int num) {
+        this.num = num;
+        den = 1;
+    }
+
+    public Fraction() {
+        num = 0;
+        den = 1;
+    }
 
     @Override
     public String toString() {
